010e08fbf0f11feb98bcbe7a5ecba4e1da113ccf
dadaloop82 <dadaloop82@gmail.com>
2025-07-23 18:02:34 +0200

    fix(config_flow): Resolve import error preventing integration load

--- custom_components/hass_ai/config_flow.py
+++ custom_components/hass_ai/config_flow.py
@@ -4,11 +4,18 @@ from homeassistant.core import callback
 from homeassistant.const import CONF_API_KEY
 
 from .const import DOMAIN, AI_PROVIDER_OPENAI, AI_PROVIDER_GEMINI, AI_PROVIDERS
+from .options_flow import HassAiOptionsFlowHandler
 
 class HassAiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Hass AI config flow."""
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return HassAiOptionsFlowHandler(config_entry)
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
@@ -29,21 +36,3 @@ class HassAiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=data_schema, errors=errors
         )
-
-@callback
-def async_get_options_flow(config_entry):
-    """Get the options flow for this handler."""
-    return HassAiOptionsFlowHandler(config_entry)
-
-class HassAiOptionsFlowHandler(config_entries.OptionsFlow):
-    """Hass AI options flow."""
-
-    def __init__(self, config_entry):
-        """Initialize HASS AI options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        # This is where you would define the options form.
-        # For now, we'll leave it empty as the next step is entity cataloging.
-        # We will come back to this later.
-        return self.async_create_entry(title="", data={})
